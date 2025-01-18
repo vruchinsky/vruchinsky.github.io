36e3589f1fdfdade705b7983ce0050230f51f6aa
Vladislav Ruchinsky <vruchinsky@gmail.com>
2025-01-17 19:25:49 -0500

    increment and decrement produce correct display in Roman numerals but bug with the pause function: pressing + or - buttons too soon (before the increment/decrement is done with all its pauses) corrupts the display. Need to disable calls to increment/decrement until the current call completes

--- TalliesToNumerals.js
+++ TalliesToNumerals.js
@@ -1,23 +1,58 @@
-const largestNumberToDisplay = 20;
+const largestNumberToDisplay = 4999;
 const romanTextElement = document.getElementById("DisplayRoman");
 
-let romanNumeralsText = "";
+//let romanNumeralsText = "";
 let inputNumber = 0;
 
-function incrementNumber() {
-	if (inputNumber > largestNumberToDisplay)
-		return;
-	inputNumber++;
-//    if (currentIndex < textToAnimate.length) {
-        romanTextElement.textContent += "I";
-//    }
+function initializeNumber() {
+	inputNumber = 0;
+	romanTextElement.textContent = "NULL";
 }
 
-function decrementNumber() {
-	if (inputNumber < 1)
+const pause = ms => new Promise(resolve => setTimeout(resolve, ms)); // from https://dev.to/rajnishkatharotiya/pause-function-execution-for-a-certain-time-in-javascript-9lj
+
+async function incrementNumber() {
+	if (inputNumber >= largestNumberToDisplay)
+		return;
+	if (inputNumber == 0)
+		romanTextElement.textContent = "";
+	inputNumber++;
+    romanTextElement.textContent += "I";
+	if (replaceLastChars("IIIII", "V")) await pause(4000);
+ 	if (replaceLastChars("VV", "X")) await pause(4000);
+	if (replaceLastChars("XXXXX", "L")) await pause(4000);
+ 	if (replaceLastChars("LL", "C")) await pause(4000);
+	if (replaceLastChars("CCCCC", "D")) await pause(4000);
+ 	replaceLastChars("DD", "M");
+}
+
+function replaceLastChars(a, b) {
+	s = romanTextElement.textContent;
+	if (s.substring(s.length-a.length) === a)
+	{
+		romanTextElement.textContent = (s.substring(0,s.length-a.length) + b);
+		return true;
+	}
+	return false;
+}
+
+async function decrementNumber() {
+	if (inputNumber <= 0)
 		return;
 	inputNumber--;
+	if (inputNumber == 0) {
+		romanTextElement.textContent = "NULL";
+		return;
+	}
+	if (replaceLastChars("M", "DD")) await pause(4000);
+	if (replaceLastChars("D", "CCCCC")) await pause(4000);
+	if (replaceLastChars("C", "LL")) await pause(4000);
+	if (replaceLastChars("L", "XXXXX")) await pause(4000);
+	if (replaceLastChars("X", "VV")) await pause(4000);
+	if (replaceLastChars("V", "IIIII")) await pause(2000);
 	let oldContent = romanTextElement.textContent;
 	let oldLength = oldContent.length;
-    romanTextElement.textContent = oldContent.substring(0,oldLength-1);
+	romanTextElement.textContent = oldContent.substring(0,oldLength-1);
 }
+
+initializeNumber();
