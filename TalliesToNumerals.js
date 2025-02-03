74ffdd4f2654dede72f4b4bb701420bd212f5d79
Vladislav Ruchinsky <vruchinsky@gmail.com>
2025-02-02 21:59:34 -0500

    adjusted code of drawBox500() to improve the look of the resulting graphic

--- TalliesToNumerals.js
+++ TalliesToNumerals.js
@@ -80,7 +80,7 @@ const vOffset = 2;
 const boundaryThickness = 1;
 const boundaryPadding = 2;
 const hSpaceInsideBox = 2;
-const longHspaceInsideBox = Math.floor(2 * hSpaceInsideBox);
+const longHspaceInsideBox = 3.6;
 const halfVspaceInsideBox = Math.ceil(vSpace / 2);
 
 function drawVline(ctx, x, y, l)
@@ -329,20 +329,22 @@ function drawBox100(ctx, x, y) // column of 50 short horizontal tally marks on t
 function drawBox500(ctx, x, y) // 10 columns each of 50 short horizontal tally marks, with extra vertical...
 {//...space between each group of 5, and extra space halfway down, all enclosed in rectangular box
 	const rnWidth = Math.floor(stringWidthOnCanvas(ctx, "D"));
-	const boxWidth = 5 * rnWidth; // 2 columns same width as 1 Roman numeral
-	const boundingRectWidth = boxWidth - 2*boundaryThickness;
-	const dHpos = (boundingRectWidth - 2*boundaryPadding)/5;
-	const lineLength = Math.floor((dHpos - 0.8*longHspaceInsideBox - hSpaceInsideBox)/2);
+	const boxWidth = Math.floor(3.9 * rnWidth);
+	const nDblCols = 5;
+	const boundingRectWidth = Math.floor(boxWidth - 2*boundaryThickness);
+	const dblColWidth = (boundingRectWidth - 2*boundaryPadding - (nDblCols-1)*longHspaceInsideBox)/nDblCols;
+	const lineLength = Math.floor((dblColWidth - hSpaceInsideBox)/2);
+	const dHpos = dblColWidth + longHspaceInsideBox;
 	let lineHpos = x + boundaryPadding + boundaryThickness; // left column horizontal position
 	const lineVpos = y + boundaryPadding + boundaryThickness; // left column vertical position
 	const columnSize = drawColumn100Hlines(ctx, lineHpos, lineVpos, lineLength);
-	lineHpos = Math.round(lineHpos + dHpos); // move horizontal position
+	lineHpos = Math.floor(lineHpos + dHpos); // move horizontal position
 	drawColumn100Hlines(ctx, lineHpos, lineVpos, lineLength);
-	lineHpos = Math.round(lineHpos + dHpos); // move horizontal position
+	lineHpos = Math.floor(lineHpos + dHpos); // move horizontal position
 	drawColumn100Hlines(ctx, lineHpos, lineVpos, lineLength);
-	lineHpos = Math.round(lineHpos + dHpos); // move horizontal position
+	lineHpos = Math.floor(lineHpos + dHpos); // move horizontal position
 	drawColumn100Hlines(ctx, lineHpos, lineVpos, lineLength);
-	lineHpos = Math.round(lineHpos + dHpos); // move horizontal position
+	lineHpos = Math.floor(lineHpos + dHpos); // move horizontal position
 	drawColumn100Hlines(ctx, lineHpos, lineVpos, lineLength);
 	const boundingRectHeight = columnSize.h + 2*boundaryPadding; // same as for 100
 	const foregroundColor = setBoxBoundaryColor(ctx, foregroundWeightBoxBoundary);
